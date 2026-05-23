36f41cd60d326b792ddc49b472ada0846c7c6f04
bishal762 <rbishal762@gmail.com>
2026-05-23 14:24:09 +0900

    Implement multiple utility functions in homework3.py
    Added functions for cancellation, copying with skip, and calculating average.

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -1,18 +1,46 @@
-
 def words2characters(words):
     """
     This function converts a list of words into a list of characters.
-
-    @param:
-    words - a list of words
-
-    @return:
-    characters - a list of characters
-
-    Every element of "words" should be converted to a str, then split into
-    characters, each of which is separately appended to "characters." For 
-    example, if words==['hello', 1.234, True], then characters should be
-    ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
     """
-    return []
+    
+    characters = []
 
+    for word in words:
+        for char in str(word):
+            characters.append(char)
+
+    return characters
+
+
+def cancellation(input_list, stop_word):
+
+    output_list = []
+
+    for item in input_list:
+        if item == stop_word:
+            break
+        output_list.append(item)
+
+    return output_list
+
+
+def copy_all_but_skip_word(input_list, skip_word):
+
+    output_list = []
+
+    for item in input_list:
+        if item == skip_word:
+            continue
+        output_list.append(item)
+
+    return output_list
+
+
+def my_average(input_list):
+
+    total = 0
+
+    for item in input_list:
+        total += item
+
+    return total / len(input_list)
