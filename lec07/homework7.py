217dab24129eba3fa180e76fd36e5a805c1b6865
bishal762 <rbishal762@gmail.com>
2026-06-03 14:10:20 +0900

    Implement major chord generation and DFT matrix

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -3,44 +3,50 @@ import numpy as np
 def major_chord(f, Fs):
     '''
     Generate a one-half-second major chord, based at frequency f, with sampling frequency Fs.
-
-    @param:
-    f (scalar): frequency of the root tone, in Hertz
-    Fs (scalar): sampling frequency, in samples/second
-
-    @return:
-    x (array): a one-half-second waveform containing the chord
-    
-    A major chord is three notes, played at the same time:
-    (1) The root tone (f)
-    (2) A major third, i.e., four semitones above f
-    (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    n = np.arange(Fs // 2)  # one-half second
+
+    f3 = f * (2 ** (4/12))  # major third
+    f5 = f * (2 ** (7/12))  # perfect fifth
+
+    x = (
+        np.cos(2 * np.pi * f * n / Fs) +
+        np.cos(2 * np.pi * f3 * n / Fs) +
+        np.cos(2 * np.pi * f5 * n / Fs)
+    )
+
+    return x
+
 
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
-    
-    @param:
-    N (scalar): number of columns in the transform matrix
-    
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    k = np.arange(N).reshape(N, 1)
+    n = np.arange(N).reshape(1, N)
+
+    W = np.cos(2 * np.pi * k * n / N) - 1j * np.sin(2 * np.pi * k * n / N)
+
+    return W.astype(complex)
+
 
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
-
-    @param:
-    x (array): the waveform
-    Fs (scalar): sampling frequency (samples/second)
-
-    @return:
-    f1, f2, f3: The three loudest frequencies (in Hertz)
-      These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    X = np.fft.fft(x)
+    mag = np.abs(X)
+
+    # Use only positive-frequency half of FFT
+    mag[len(mag)//2:] = 0
+
+    freqs = []
+
+    for _ in range(3):
+        idx = np.argmax(mag)
+        freqs.append(idx * Fs / len(x))
+        mag[idx] = 0
+
+    freqs.sort()
+
+    return freqs[0], freqs[1], freqs[2]
