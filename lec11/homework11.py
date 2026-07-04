76ce080f19e6ee913ec7677d074a7aebf4d377ef
bishal762 <rbishal762@gmail.com>
2026-07-04 12:24:27 +0900

    Add default language parameter to transcribe_wavefile
    Updated transcribe_wavefile function to have a default language parameter and implemented the speech recognition logic.

--- lec11/homework11.py
+++ lec11/homework11.py
@@ -1,17 +1,22 @@
 import speech_recognition as sr
 
-def transcribe_wavefile(filename, language):
+def transcribe_wavefile(filename, language='en'):
     '''
     Use sr.Recognizer.AudioFile(filename) as the source,
     recognize from that source,
     and return the recognized text.
-    
+
     @params:
     filename (str) - the filename from which to read the audio
-    language (str) - the language of the audio
-    
+    language (str) - the language of the audio (optional; default is English)
+
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
-        
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+
+    text = recognizer.recognize_google(audio, language=language)
+    return text
